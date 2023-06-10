b6c568640f7bdfee770135084a9799edbbb9b4ae
Indrajit Chakrabarty <indyfromoz@gmail.com>
2023-06-11 07:05:26 +1000

    Completed Retrofit callback implementation of UseCase 2 - sequential network calls

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/callbacks/SequentialNetworkRequestsCallbacksViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/callbacks/SequentialNetworkRequestsCallbacksViewModel.kt
@@ -1,12 +1,62 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2.callbacks
 
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SequentialNetworkRequestsCallbacksViewModel(
     private val mockApi: CallbackMockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
-    fun perform2SequentialNetworkRequest() {
+    private var getAndroidVersionsCall: Call<List<AndroidVersion>>? = null
+    private var getAndroidFeaturesCall: Call<VersionFeatures>? = null
 
+    fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+
+        getAndroidVersionsCall = mockApi.getRecentAndroidVersions()
+        getAndroidVersionsCall!!.enqueue(object : Callback<List<AndroidVersion>> {
+            override fun onResponse(
+                call: Call<List<AndroidVersion>>,
+                response: Response<List<AndroidVersion>>
+            ) {
+                if (response.isSuccessful && response.body() != null) {
+                    val mostRecentVersion = response.body()!!.last()
+                    getAndroidFeaturesCall = mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+                    getAndroidFeaturesCall!!.enqueue(object : Callback<VersionFeatures> {
+                        override fun onResponse(
+                            call: Call<VersionFeatures>,
+                            response: Response<VersionFeatures>
+                        ) {
+                            if (response.isSuccessful && response.body() != null) {
+                                uiState.value = UiState.Success(response.body()!!)
+                            } else {
+                                uiState.value = UiState.Error("Network Request failed")
+                            }
+                        }
+
+                        override fun onFailure(call: Call<VersionFeatures>, t: Throwable) {
+                            uiState.value = UiState.Error("Something went wrong")
+                        }
+                    })
+                } else {
+                    uiState.value = UiState.Error("Network Request failed")
+                }
+            }
+
+            override fun onFailure(call: Call<List<AndroidVersion>>, t: Throwable) {
+                uiState.value = UiState.Error("Something went wrong")
+            }
+        })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        getAndroidVersionsCall?.cancel()
+        getAndroidFeaturesCall?.cancel()
     }
 }
