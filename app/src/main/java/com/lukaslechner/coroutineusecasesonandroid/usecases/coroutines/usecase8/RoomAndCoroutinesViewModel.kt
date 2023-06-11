da40fea2aa69eecda46429e4d6cfe2404fb3c200
Indrajit Chakrabarty <indyfromoz@gmail.com>
2023-06-12 09:15:34 +1000

    Completed Coroutines implementation of Room database loading data with Coroutines in usecase#8

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
@@ -1,7 +1,9 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase8
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class RoomAndCoroutinesViewModel(
     private val api: MockApi,
@@ -9,11 +11,35 @@ class RoomAndCoroutinesViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun loadData() {
+        uiState.value = UiState.Loading.LoadFromDb
 
+        viewModelScope.launch {
+            val localAndroidVersions = database.getAndroidVersions()
+            if (localAndroidVersions.isNotEmpty()) {
+                uiState.value =
+                    UiState.Success(DataSource.DATABASE, localAndroidVersions.mapToUiModelList())
+            } else {
+                uiState.value = UiState.Error(DataSource.DATABASE, "Database is empty")
+            }
+
+            uiState.value = UiState.Loading.LoadFromNetwork
+            try {
+                val recentVersions = api.getRecentAndroidVersions()
+                recentVersions.forEach { androidVersion ->
+                    database.insert(androidVersion.mapToEntity())
+                }
+                uiState.value = UiState.Success(DataSource.NETWORK, recentVersions)
+            } catch (exception: Exception) {
+                uiState.value =
+                    UiState.Error(DataSource.NETWORK, exception.message ?: "Something went wrong")
+            }
+        }
     }
 
     fun clearDatabase() {
-
+        viewModelScope.launch {
+            database.clear()
+        }
     }
 }
 
