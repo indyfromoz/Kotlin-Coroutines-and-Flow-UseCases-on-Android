ca114932cfb8732ca8e574496766bdcea06646a6
Indrajit Chakrabarty <indyfromoz@gmail.com>
2023-06-12 12:40:23 +1000

    Completed Coroutines implementation of a long-running operatio with a Application level coroutine scope with a SupervisorJob in usecsase#14

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/AndroidVersionRepository.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/AndroidVersionRepository.kt
@@ -3,22 +3,31 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class AndroidVersionRepository(
     private var database: AndroidVersionDao,
     private val scope: CoroutineScope,
     private val api: MockApi = mockApi()
 ) {
-
     suspend fun getLocalAndroidVersions(): List<AndroidVersion> {
         return database.getAndroidVersions().mapToUiModelList()
     }
 
     suspend fun loadAndStoreRemoteAndroidVersions(): List<AndroidVersion> {
-        return emptyList()
+        return scope.async {
+            val recentVersions = api.getRecentAndroidVersions()
+            recentVersions.forEach { version ->
+                database.insert(version.mapToEntity())
+            }
+            recentVersions
+        }.await()
     }
 
     fun clearDatabase() {
-
+        scope.launch {
+            database.clear()
+        }
     }
 }
